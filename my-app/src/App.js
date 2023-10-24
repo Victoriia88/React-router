929fb4aa00d0eb4a4a7dc0a7081af74c49044ba5
Victoriia Ulynets <123025408+Victoriia88@users.noreply.github.com>
2023-10-24 23:42:02 +0300

    hw43

---
+++ my-app/src/App.js
@@ -0,0 +1,37 @@
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import UserList from "./UserList";
+import UserDetails from "./UserDetails";
+import AlbumList from "./AlbumList";
+import AlbumDetails from "./AlbumDetails"; // Додайте цей імпорт
+import PhotoList from "./PhotoList";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        <div className="sidebar">
+          <Routes>
+            <Route path="/" element={<UserList />} />
+            <Route path="/users/:userId" element={<UserDetails />}>
+              <Route path="albums" element={<AlbumList />} />
+              <Route path="photos" element={<PhotoList />} />
+            </Route>
+          </Routes>
+        </div>
+        <div className="main-content">
+          <Routes>
+            <Route
+              path="/users/:userId/albums/:albumId"
+              element={<AlbumDetails />}
+            />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
